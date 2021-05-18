c6d9e6e6e4eacb3ebbcf00d4a6ce01ae7c20e7c4
SandroGT <sandrogabrieletiddia@outlook.it>
2021-05-18 22:59:34 +0200

    Working over the querying algorithm
    (still in progress)

--- algorithms/trapezoidalmap_alg.cpp
+++ algorithms/trapezoidalmap_alg.cpp
@@ -1,12 +1,43 @@
 #include "trapezoidalmap_alg.h"
 
+#include <cg3/geometry/utils2.h>
+
 namespace gasprj {
 
 void addSegmentToTrapezoidalMap() {
 
 }
 
-void queryTrapezoidalMap() {
+size_t queryTrapezoidalMap(const cg3::Point2d& point, const TrapezoidalMapDataset& trapMapData,
+                           const gasprj::DAG& dag)
+{
+    DAGNode dagNode = dag.getRoot();
+
+    // Scroll the DAG until a leaf is reached
+    while(dagNode.getNodeType() != gasprj::NodeType::TrapNode) {
+        // Check if the actual node is a x-node (vertex) or a y-node (segment)
+        switch(dagNode.getNodeType())
+        {
+        case gasprj::NodeType::XNode:
+            // Point-Vertex comparison
+            if () {
+
+            }
+            break;
+        case gasprj::NodeType::YNode:
+            // Point-Segment comparison
+            if () {
+
+            }
+            break;
+        default:
+            // Unreachable
+            assert(false);
+        } // End switch
+    }
+
+    // At this point the node must be a leaf
+    assert(dagNode.getNodeType() == gasprj::NodeType::TrapNode)
 
 }
 
--- algorithms/trapezoidalmap_alg.h
+++ algorithms/trapezoidalmap_alg.h
@@ -1,10 +1,16 @@
 #ifndef TRAPEZOIDALMAP_ALG_H
 #define TRAPEZOIDALMAP_ALG_H
 
+#include <cg3/geometry/segment2.h>
+
+#include "data_structures/dag.h"
+#include "drawables/drawable_trapezoidalmap_dataset.h"
+
 namespace gasprj {
 
 void addSegmentToTrapezoidalMap();
-void queryTrapezoidalMap();
+size_t queryTrapezoidalMap(const cg3::Point2d& point, const TrapezoidalMapDataset& trapMapData,
+                           const gasprj::DAG& dag);
 
 } // End namespace gasprj
 
