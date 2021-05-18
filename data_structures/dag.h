a4c5e4066b10205bc4c9b2c887579cba47c3e3b2
SandroGT <sandrogabrieletiddia@outlook.it>
2021-05-18 21:43:25 +0200

    First attempt with the definition of the DAG structure
    A class is created to define a DAG node, storing its type (enumeration), a reference to the information contained by that node (an index for other data structures) and the indexes for its left and right nodes. All the nodes are stored and handled with a vector, expecting to only have addition of new nodes with no care for their order in the vector.

--- data_structures/dag.h
+++ data_structures/dag.h
@@ -1,11 +1,41 @@
 #ifndef DAG_H
 #define DAG_H
 
+#include <vector>
+
+namespace gasprj {
+
+enum class NodeType {XNode, YNode, TrapNode};
+
+class DAGNode
+{
+public:
+    // TODO: define constructor
+    DAGNode();
+
+    NodeType getNodeType();
+    size_t getIdxInfo();
+    size_t getIdxNodeLeft();
+    size_t getIdxNodeRight();
+
+private:
+    NodeType typeInfo;
+    size_t idxInfo;
+    size_t idxNodeLeft, idxNodeRight;
+};
 
 class DAG
 {
 public:
+    // TODO: define constructor
     DAG();
+
+    const DAGNode& getRoot();
+
+private:
+    std::vector<DAGNode> nodes;
 };
+
+} // End namespace gasprj
 
 #endif // DAG_H
